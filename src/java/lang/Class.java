9396999a9b889ee1480b72114624aee8cdb685e7
yang.jin <yang.jin@msxf.com>
2018-02-26 22:03:16 +0800

    量变引起质变

---
+++ src/java/lang/Class.java
@@ -0,0 +1,6 @@
+package java.lang;
+
+import java.io.Serializable;
+
+public final class Class<T> implements Serializable {
+}
--- src/java/lang/Object.java
+++ src/java/lang/Object.java
@@ -1,15 +1,34 @@
 package java.lang;
 
+import jdk.internal.HotSpotIntrinsicCandidate;
+
 public class Object {
 
+    /**
+     * 调用这个方法后，JNI会开始工作，调用src\java.base\share\native\libjava\Object.c文件（仅可在openjdk9中看到）
+     */
     private static native void registerNatives();
     static {
         registerNatives();
     }
 
+    /**
+     * 为什么从9开始要把这个隐藏构造函数显示出来，就是为了搭配HotSpotIntrinsicCandidate完成函数内联，极速优化！
+     */
+    @HotSpotIntrinsicCandidate
     public Object() {
 
     }
+
+    @HotSpotIntrinsicCandidate
+    public final native Class<?> getClass();
+
+
+
+
+
+
+
 
     public boolean equals(Object o) {
         return o == this;
